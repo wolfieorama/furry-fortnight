11a8d0b3ea2ffc7cb370d97c2b3e85440d1ca06c
wolfieorama <wachirawamunyi@gmail.com>
2016-04-13 15:21:40 +0300

    creating notifications after posts

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,66 +1,20 @@
 class ForumPostsController < ApplicationController
-  # before_action :set_forum_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
   before_action :set_forum_thread
 
-  # GET /forum_posts
-  # GET /forum_posts.json
-  def index
-    @forum_posts = ForumPost.all
-  end
-
-  # GET /forum_posts/1
-  # GET /forum_posts/1.json
-  def show
-  end
-
-  # GET /forum_posts/new
-  def new
-    @forum_post = ForumPost.new
-  end
-
-  # GET /forum_posts/1/edit
-  def edit
-  end
-
-  # POST /forum_posts
-  # POST /forum_posts.json
   def create
     @forum_post = @forum_thread.forum_posts.new(forum_post_params)
     @forum_post.user = current_user
+    if @forum_post.save
 
-    respond_to do |format|
-      if @forum_post.save
-        format.html { redirect_to @forum_thread, notice: 'Forum post was successfully created.' }
-        format.json { render :show, status: :created, location: @forum_post }
-      else
-        format.html { render :new }
-        format.json { render json: @forum_post.errors, status: :unprocessable_entity }
+      # Create the notifications
+      (@forum_thread.users.uniq - [current_user]).each do |user|
+        Notification.create(recipient: user, actor: current_user, action: "posted", notifiable: @forum_post)
       end
-    end
-  end
 
-
-  # PATCH/PUT /forum_posts/1
-  # PATCH/PUT /forum_posts/1.json
-  def update
-    respond_to do |format|
-      if @forum_post.update(forum_post_params)
-        format.html { redirect_to @forum_post, notice: 'Forum post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @forum_post }
-      else
-        format.html { render :edit }
-        format.json { render json: @forum_post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /forum_posts/1
-  # DELETE /forum_posts/1.json
-  def destroy
-    @forum_post.destroy
-    respond_to do |format|
-      format.html { redirect_to forum_posts_url, notice: 'Forum post was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to @forum_thread
+    else
+      render "forum_threads/show"
     end
   end
 
@@ -69,13 +23,8 @@ class ForumPostsController < ApplicationController
     def set_forum_thread
       @forum_thread = ForumThread.find(params[:forum_thread_id])
     end
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_forum_post
-    #   @forum_post = ForumPost.find(params[:id])
-    # end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def forum_post_params
-      params.require(:forum_post).permit(:body, :user_id, :forum_thread_id)
+      params.require(:forum_post).permit(:body)
     end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ActiveRecord::Base
   belongs_to :recepient, class_name: "User"
   belongs_to :actor, class_name: "User"
-  belongs_to :notifiabe, polymorphic: true
+  belongs_to :notifiable, polymorphic: true
 end
